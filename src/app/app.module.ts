f2c3811b70ef288c49bf47fd1cc2b99af4cbf4cc
Hugo Regadas <dev.hugoregadas@gmail.com>
2022-03-31 12:20:32 +0200

    chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+import { LogInComponent } from './pages/log-in/log-in.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { OverviewComponent } from './pages/overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    ChatComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
